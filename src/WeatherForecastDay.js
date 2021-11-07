e2d6c00ec3ed1e697fe16205eff6609dfefaa25e
LuziaFernandes <87724388+LuziaFernandes@users.noreply.github.com>
2021-11-07 12:15:22 +0000

    Fixed Icon display on the daily forecast

--- src/WeatherForecastDay.js
+++ src/WeatherForecastDay.js
@@ -2,6 +2,9 @@ import React from "react";
 
 
 export default function WeatherForecastDay (props) { 
+
+    let weatherIcon = `https://www.openweathermap.org/img/wn/${props.data.weather[0].icon}@2x.png`;
+
 function maxTemperature() {
     let temperature = Math.round(props.data.temp.max);
     return `${temperature}º`;
@@ -12,6 +15,7 @@ function minTemperature() {
 }
 
 function day(){
+
     let date = new Date (props.data.dt * 1000);
     let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
     let day = date.getDay();
@@ -20,8 +24,9 @@ function day(){
 }
     return (
         <div className="WeatherForecastDay">
-    <div className="day"> {day()} </div>
-<div className="emoji"><img src="https://www.openweathermap.org/img/wn/10d@2x.png" alt={props.data.weather[0].description} /> </div>
+            
+    <div className="day"> {day} </div>
+<div className="emoji"><img src= {weatherIcon} alt={props.data.weather[0].description} /> </div>
 
 <div className="temperatures-of-day"><span className="temperature-day-max"> {maxTemperature()} </span><span className="temperature-day-min"> {minTemperature()} </span></div>
 
